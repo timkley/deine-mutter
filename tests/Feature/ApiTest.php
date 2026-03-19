2e8828f391aad48cca4e933ca0c14545074adb76
Tim Kleyersburg <tk@wacg.de>
2026-03-19 06:48:43 +0100

    upgrade to PHP 8.5 and Laravel 12
    - Update PHP requirement to ^8.5 in composer.json and CI workflow
    - Upgrade laravel/framework from ^11.31 to ^12.0
    - Replace deprecated PDO::MYSQL_ATTR_SSL_CA with Pdo\Mysql::ATTR_SSL_CA
    - Fix test formatting (pint)

--- tests/Feature/ApiTest.php
+++ tests/Feature/ApiTest.php
@@ -5,6 +5,6 @@ declare(strict_types=1);
 use function Pest\Laravel\get;
 
 test('it returns a random joke', function () {
-get('/api')
-->assertJsonStructure(['joke']);
-    });
+    get('/api')
+        ->assertJsonStructure(['joke']);
+});
